baa504c0ad477fa076c861c62601be91a8d0eb9b
Tyler Polen <tyler.polen@gmail.com>
2020-05-15 19:02:57 -0600

    add missing files

---
+++ vue/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <LoginButtons provider="spotify"/>
+    <LoginButtons provider="discord"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginButtons from '@/components/LoginButtons.vue';
+
+export default {
+  name: 'home',
+  components: {
+    LoginButtons,
+  },
+};
+</script>
